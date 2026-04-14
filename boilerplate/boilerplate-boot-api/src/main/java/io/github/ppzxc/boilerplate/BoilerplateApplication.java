49983bd88d66031df99c37eb725265c882b2da8b
ppzxc <cjh8487@naver.com>
2026-04-14 11:12:49 +0900

    feat(boot): add @Modulithic annotation to BoilerplateApplication (E-2/I-1)

--- boilerplate/boilerplate-boot-api/src/main/java/io/github/ppzxc/boilerplate/BoilerplateApplication.java
+++ boilerplate/boilerplate-boot-api/src/main/java/io/github/ppzxc/boilerplate/BoilerplateApplication.java
@@ -2,8 +2,10 @@ package io.github.ppzxc.boilerplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.modulith.Modulithic;
 
 /** Spring Boot API 애플리케이션 진입점. */
+@Modulithic(systemName = "Boilerplate")
 @SpringBootApplication
 public class BoilerplateApplication {
 
